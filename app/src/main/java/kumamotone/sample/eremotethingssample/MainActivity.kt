6585a8c4820856fffe58ffd139373f0117626139
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2018-03-20 03:51:59 +0900

    Add impl

--- app/src/main/java/kumamotone/sample/eremotethingssample/MainActivity.kt
+++ app/src/main/java/kumamotone/sample/eremotethingssample/MainActivity.kt
@@ -2,30 +2,142 @@ package kumamotone.sample.eremotethingssample
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.DatagramChannel
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * val service = PeripheralManagerService()
- * val mLedGpio = service.openGpio("BCM6")
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * mLedGpio.value = true
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- *
- */
 class MainActivity : Activity() {
+    private fun String.hexStringToByteArray(): ByteArray =
+            ByteArray(this.length / 2) { this.substring(it * 2, it * 2 + 2).toInt(16).toByte() }
+
+    private fun sendToRemocon(data: String) {
+        launch {
+            sendToRemoconWithoutCoroutine(data)
+        }
+    }
+
+    private fun sendToRemoconWithoutCoroutine(data: String) {
+        DatagramChannel.open().use {
+            it.socket().bind(InetSocketAddress(9999 /* 送り元ポート */))
+            val buf = ByteBuffer.allocate(data.length).also {
+                it.clear()
+                it.put(data.hexStringToByteArray())
+                it.flip()
+            }
+            it.send(buf, InetSocketAddress(getString(R.string.ipaddress), 80))
+        }
+    }
+
+    private fun changeLight(words: List<String>) {
+        when (words.getOrNull(1)) {
+            "の", "を" -> {
+                changeLight(words.subList(1, words.count()))
+            }
+            "つけ", "つけて", "付け", "付けて" -> {
+                sendToRemocon(getString(R.string.light_on))
+            }
+            "けし", "けして", "消し", "消して" -> {
+                sendToRemocon(getString(R.string.light_off))
+            }
+            else -> {
+                return
+            }
+        }
+    }
+
+    private fun changeAircon(words: List<String>) {
+        when (words.getOrNull(1)) {
+            "の", "を" -> {
+                changeAircon(words.subList(1, words.count()))
+            }
+            "つけ", "つけて", "付け", "付けて" -> {
+                sendToRemocon(getString(R.string.heat))
+            }
+            "けし", "けして", "消し", "消して" -> {
+                sendToRemocon(getString(R.string.aircon_off))
+            }
+            "除湿" -> {
+                sendToRemocon(getString(R.string.humid))
+            }
+            else -> {
+                return
+            }
+        }
+    }
+
+    private fun oyasumi() {
+        launch {
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.light_off))
+            }.await()
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.aircon_off))
+            }.await()
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.timer7hours))
+            }
+        }
+    }
+
+    private fun imhome() {
+        launch {
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.light_on))
+            }.await()
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.heat))
+            }.await()
+        }
+    }
+
+    private fun imgoing() {
+        launch {
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.light_off))
+            }.await()
+            async {
+                sendToRemoconWithoutCoroutine(getString(R.string.aircon_off))
+            }.await()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val database = FirebaseDatabase.getInstance()
+        val reference = database.getReference("googlehome").child("word")
+        reference.setValue("")
+
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val str = dataSnapshot.getValue(String::class.java) ?: return
+                val words = str.split(" ")
+
+                when (words.first()) {
+                    "light" -> {
+                        changeLight(words)
+                    }
+                    "aircon" -> {
+                        changeAircon(words)
+                    }
+                    "oyasumi" -> {
+                        oyasumi()
+                    }
+                    "imgoing" -> {
+                        imgoing()
+                    }
+                    "imhome" -> {
+                        imhome()
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
     }
 }
